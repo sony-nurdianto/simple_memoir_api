4b81f847e9317b67346ec8d10ec7e6118f1e1516
sony-nurdianto <sonynurdianto445@gmail.com>
2020-06-07 10:46:46 +0700

    seconds commit

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require("body-parser");
 var logger = require('morgan');
+const mongoose = require("mongoose");
+var cors = require("cors"); 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/Users');
 
 var app = express();
+mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+
+app.use(cors())
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
